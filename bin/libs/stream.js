da0cf637a01185404ecdc1ffe8275a03fadd77a8
uselessdev <wb_art@hotmail.com>
2016-05-29 02:23:45 -0300

    Fix: peerlix not found

--- bin/libs/stream.js
+++ bin/libs/stream.js
@@ -4,10 +4,12 @@
 module.exports = function (torrent, subtitle) {
   var colors = require('colors');
   var shell = require('shelljs');
+  var path = require('path');
+  var peerflixPath = path.resolve(__dirname + '/../../node_modules/peerflix/app.js');
 
   console.log('downloading...'.magenta);
 
-  var execStr = 'node ./node_modules/peerflix/app.js "' + torrent + '" --vlc';
+  var execStr = 'node '+ peerflixPath +' "' + torrent + '" --vlc';
 
   if (subtitle !== undefined) {
     execStr += ' -- --sub-file=' + subtitle;
